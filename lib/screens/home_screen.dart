2ac1c63de11bc8d98c9fc5e1025de00246d778b0
sundarlucifer <sundarlucifer@gmail.com>
2020-02-19 11:55:19 +0530

    home page & hosting

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,81 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget{
-
+class HomeScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _HomeScreenState();
-
 }
 
-class _HomeScreenState extends State<HomeScreen>{
-
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Welcome to Movies Repo'),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image(
+                  height: 100.0,
+                  width: 100.0,
+                  image: AssetImage('assets/images/movie_01.png'),
+                ),
+                Text(
+                  'Movies Repo',
+                  style: TextStyle(
+                    fontSize: 50.0,
+                    // color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              width: 500,
+              height: 100.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15.0),
+                // boxShadow: [
+                //   BoxShadow(
+                //     blurRadius: 6.0,
+                //     offset: Offset(1, 0),
+                //     color: Colors.black12,
+                //   ),
+                // ],
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    width: 300.0,
+                    child: TextField(
+                      showCursor: false,
+                      onSubmitted: (String title){},
+                      decoration: InputDecoration(
+                          hintText: 'Type title of movie',
+                          hintStyle: TextStyle(color: Colors.black38)),
+                    ),
+                  ),
+                  FlatButton(
+                    color: Theme.of(context).highlightColor,
+                    child: Text(
+                      'SEARCH',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
-
 }
---
+++ site/flutter_service_worker.js
@@ -0,0 +1,42 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "/index.html": "973d5db91d5e96e70c60829b384c5e15",
+"/main.dart.js": "55ddba62ad52bdfd9f24cd7345d1cafb",
+"/favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/manifest.json": "24bbfef877ea4cea023d632582eaa609",
+"/assets/LICENSE": "d5e242e0748806a5f7eb2065c5bbc176",
+"/assets/AssetManifest.json": "338fd7a051a451126e46ca3c319fd03f",
+"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/assets/images/movie_01.png": "c8397bb501fb4a5c8018a93cd2f4f2bb"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request, {
+          credentials: 'include'
+        });
+      })
+  );
+});
